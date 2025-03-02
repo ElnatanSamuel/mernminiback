a7f0b88ae40ada6abdf628fa2ab6ebb8a4e7eade
EL <elnatansamuel22@gmail.com>
2025-03-02 06:05:44 +0300

    back

--- server.js
+++ server.js
@@ -3,19 +3,41 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Connect to MongoDB with error handling
 mongoose.connect("mongodb+srv://ktk2real:krosection999@cluster0.abfalpl.mongodb.net/internshiptest?retryWrites=true&w=majority")
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => {
     console.error('MongoDB connection error:', err);
     process.exit(1);
   });
+
+
+const corsOptions = {
+    origin: ["http://localhost:3000", "https://mernminifront.vercel.app"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  };
+  
+
+const app = express()
+
+// Middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://mernminifront.vercel.app");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+  });
+  
+
+// Connect to MongoDB with error handling
 
 // Routes
 app.use('/api', require('./routes/index'));
