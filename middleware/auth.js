8be6c6600c067d69d9f36464ec730e4715c48df0
EL <elnatansamuel22@gmail.com>
2025-03-02 06:29:22 +0300

    i

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = 'yjwt_5v8x/A?D(G+KbPeShVmYq3t6w9z$C&F)'; // Hardcode for testing
+const JWT_SECRET = 'jwt_5v8x/A?D(G+KbPeShVmYq3t6w9z$C&F)';
 
 const auth = (req, res, next) => {
   try {
